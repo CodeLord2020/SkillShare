e805ca47c81651dfa74d56ba8c50a41478d03710
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-14 08:30:06 +0100

    SkillCategory

---
+++ Domain/Enums/SkillCategory.cs
@@ -0,0 +1,12 @@
+namespace SkillShare.Domain.Enums
+{
+    public enum SkillCategory
+    {
+        Technology,
+        Art,
+        Cooking,
+        Gardening,
+        Fitness,
+        Other
+    }
+}
