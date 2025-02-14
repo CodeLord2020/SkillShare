f8a57f78a41e088a14a32ac7cc31efd76136fb91
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-14 08:26:10 +0100

    User repo+service injection

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using SkillShare.Application.Interfaces;
+using SkillShare.Application.Services;
 using SkillShare.Persistence.Data;
+using SkillShare.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +10,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
     
 // Add services to the container.
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
