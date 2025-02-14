76c830bd1fae0a2abe5b27c4058e132793c7312a
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-14 08:13:30 +0100

    IUserService

---
+++ Application/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SkillShare.Domain.Entities;
+
+namespace SkillShare.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(Guid id);
+        Task<User> GetUserByEmailAsync(string email);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(Guid id);
+    }
+}
