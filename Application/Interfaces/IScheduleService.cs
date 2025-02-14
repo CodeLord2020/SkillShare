a71b8889aa4c9d6706094cb6b58538bcf38a6995
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-14 17:17:22 +0100

    Trade+Schedule Iserveice

---
+++ Application/Interfaces/IScheduleService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SkillShare.Domain.Entities;
+
+namespace SkillShare.Application.Interfaces
+{
+    public interface IScheduleService
+    {
+        Task<IEnumerable<Schedule>> GetAllSchedulesAsync();
+        Task<Schedule> GetScheduleByIdAsync(Guid id);
+        Task<IEnumerable<Schedule>> GetSchedulesByUserIdAsync(Guid userId);
+        Task AddScheduleAsync(Schedule schedule);
+        Task UpdateScheduleAsync(Schedule schedule);
+        Task DeleteScheduleAsync(Guid id);
+    }
+}
