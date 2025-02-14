d5d14898e851ef0f35cae9084ad99eae6a1eb4f1
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-14 08:52:47 +0100

    cvISkillRepository

---
+++ Application/Interfaces/ISkillRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SkillShare.Domain.Entities;
+
+namespace SkillShare.Application.Interfaces
+{
+    public interface ISkillRepository
+    {
+        Task<IEnumerable<Skill>> GetAllSkillsAsync();
+        Task<Skill> GetSkillByIdAsync(Guid id);
+        Task<IEnumerable<Skill>> GetSkillsByUserIdAsync(Guid userId);
+        Task AddSkillAsync(Skill skill);
+        Task UpdateSkillAsync(Skill skill);
+        Task DeleteSkillAsync(Guid id);
+    }
+}
