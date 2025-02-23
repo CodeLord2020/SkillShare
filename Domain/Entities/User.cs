2a69a0eb5ad19f832eb89c57c246544985bad702
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-23 17:25:09 +0100

    IdentityUser model

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -2,26 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using SkillShare.Domain.Enums;
 
 namespace SkillShare.Domain.Entities
 {
-    public class User
+    public class User : IdentityUser<Guid>
     {
-        public Guid Id { get; set; }
         public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required string Email { get; set; }
-        public required string PasswordHash { get; set; }    
+        public required string LastName { get; set; }  
         public UserRoles Role { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? LastUpdatedAt { get; set;}
         public bool IsVerified { get; set; }
-        public int ReputationScore { get; set; }
+        public int ReputationScore { get; set; } = 0;
 
 
         // Navigationa Properties.
-
         public ICollection<Skill> Skills { get; set; } = new List<Skill>();
         public ICollection<Trade> TradesAsInitiator { get; set; } = new List<Trade>();
         public ICollection<Trade> TradesAsReceiver { get; set; } = new List<Trade>();   
