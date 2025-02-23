d512f26e47fcf29648660541e41b862672a47919
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-23 17:25:26 +0100

    IdentityUser AppdbContext

--- Persistence/Data/AppDbContext.cs
+++ Persistence/Data/AppDbContext.cs
@@ -1,14 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SkillShare.Domain.Entities;
 using SkillShare.Persistence.Configuration;
 namespace SkillShare.Persistence.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext :IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
         {}
         
-        public DbSet<User> Users { get; set; }
+        // public DbSet<User> Users { get; set; }
         public DbSet<Trade> Trades { get; set; }
         public DbSet<Schedule> Schedules { get; set; }
         public DbSet<Reputation> Reputations { get; set; }
