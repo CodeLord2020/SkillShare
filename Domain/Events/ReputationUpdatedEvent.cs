786f2b349e8ac1802f6935f7123e20d7d644249e
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-25 10:26:24 +0100

    ReputationUpdatedEvent

--- Domain/Events/ReputationUpdatedEvent.cs
+++ Domain/Events/ReputationUpdatedEvent.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SkillShare.Domain.Entities;
 
 namespace SkillShare.Domain.Events
 {
     public class ReputationUpdatedEvent
     {
-        
+        public User User { get; }
+
+        public ReputationUpdatedEvent(User user)
+        {
+            User = user;
+            
+        }
     }
 }
