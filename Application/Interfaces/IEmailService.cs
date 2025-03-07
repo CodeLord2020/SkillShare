8f65e7a4a31e3befda62d2b3357dd2f558444171
Rasheed Babatunde <brasheed240@gmail.com>
2025-03-07 12:00:38 +0100

    SendGridEmailService

--- Application/Interfaces/IEmailService.cs
+++ Application/Interfaces/IEmailService.cs
@@ -7,6 +7,6 @@ namespace SkillShare.Application.Interfaces
 {
     public interface IEmailService
     {
-        
+        Task SendEmailAsync(string toEmail, string subject, string plainTextContent, string htmlContent);
     }
 }
--- Application/Services/SendGridEmailService.cs
+++ Application/Services/SendGridEmailService.cs
@@ -1,12 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using SkillShare.Application.Interfaces;
 using System.Threading.Tasks;
 
 namespace SkillShare.Application.Services
 {
-    public class SendGridEmailService
+    public class SendGridEmailService : IEmailService
     {
-        
+         private readonly ISendGridClient _sendGridClient;
+        private readonly string _fromEmail;
+        private readonly string _fromName;
+
+    }
+
+    public SendGridEmailService(ISendGridClient sendGridClient, IConfiguration configuration)
+        {
+            _sendGridClient = sendGridClient;
+            _fromEmail = configuration["SendGrid:FromEmail"];
+            _fromName = configuration["SendGrid:FromName"];
+        }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string plainTextContent, string htmlContent)
+        {
+            var from = new EmailAddress(_fromEmail, _fromName);
+            var to = new EmailAddress(toEmail);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            await _sendGridClient.SendEmailAsync(msg);
+        }
     }
 }
