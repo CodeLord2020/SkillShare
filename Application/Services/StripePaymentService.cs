e93e5770d50a5141047fbe02ec2b648e3142e89d
Rasheed Babatunde <brasheed240@gmail.com>
2025-03-07 11:40:34 +0100

    StripePaymentService

--- Application/Services/StripePaymentService.cs
+++ Application/Services/StripePaymentService.cs
@@ -8,14 +8,53 @@ namespace SkillShare.Application.Services
 {
     public class StripePaymentService : IPaymentService
     {
-        public Task<string> CraeteCheckoutSessionAsync(decimal amount, string currency, string suceessUrl, string cancelUrl)
+        private readonly IConfiguration _configuration;
+        public StripePaymentService(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+            StripeConfiguration.ApiKey =  _configuration.GetSection("Stripe")["SecretKey"];
+            
+        }
+        public async Task<string> CraeteCheckoutSessionAsync(decimal amount, string currency, 
+        string suceessUrl, string cancelUrl)
+        {
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string> { "card" },
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(amount * 100), // Amount in cents
+                            Currency = currency,
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = "Premium Profile Subscription",
+                            },
+                        },
+                        Quantity = 1,
+                    },
+                },
+                Mode = "payment",
+                SuccessUrl = successUrl,
+                CancelUrl = cancelUrl,
+            };
+
+            var service = new SessionService();
+            var session = await service.CreateAsync(options);
+
+            return session.Id;
         }
 
-        public Task<bool> ProcessPaymentAsync(string paymentIntentId)
+
+        public async Task<bool> ProcessPaymentAsync(string paymentIntentId)
         {
-            throw new NotImplementedException();
+            var service = new PaymentIntentService();
+            var paymentIntent = await service.GetAsync(paymentIntentId);
+
+            return paymentIntent.Status == "succeeded";
         }
     }
 }
