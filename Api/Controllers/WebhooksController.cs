67288485df7d906c4937af78fc64487b5b7e649d
Rasheed Babatunde <brasheed240@gmail.com>
2025-03-07 11:51:10 +0100

    stripe WebhooksController

--- Api/Controllers/WebhooksController.cs
+++ Api/Controllers/WebhooksController.cs
@@ -13,12 +13,12 @@ namespace SkillShare.Api.Controllers
         public readonly IConfiguration _configuration;
         public WebhooksController(IConfiguration configuration)
         {
-          _configuration = configuration;   
+          _configuration = configuration;  
         }
         [HttpPost("stripe")]
         public async Task<IActionResult> StripeWebhook()
         {
             var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], "your_stripe_webhook_secret");
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], _configuration.GetSection("Stripe")["SecreteKey"]);
     }
 }
