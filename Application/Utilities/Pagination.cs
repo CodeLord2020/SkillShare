cc9d0e19109ace6a8b86114fded9bdfcc709966c
Rasheed Babatunde <brasheed240@gmail.com>
2025-03-14 18:02:24 +0100

    Pagination

--- Application/Utilities/Pagination.cs
+++ Application/Utilities/Pagination.cs
@@ -5,8 +5,21 @@ using System.Threading.Tasks;
 
 namespace SkillShare.Application.Utilities
 {
-    public class Pagination
+    public class Pagination<T>
     {
-        
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public List<T> Items { get; set; } = new();
+    
+
+    public Pagination(int pageNumber, int pageSize, int totalCount, List<T> items)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+            Items = items;
+        }
     }
 }
+
