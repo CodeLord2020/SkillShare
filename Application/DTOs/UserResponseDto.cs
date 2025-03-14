349fb4178789b8d4203119e0954b5d8454dfd3d3
Rasheed Babatunde <brasheed240@gmail.com>
2025-03-14 17:50:50 +0100

    UserResponseDto

--- Application/DTOs/UserResponseDto.cs
+++ Application/DTOs/UserResponseDto.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SkillShare.Domain.Enums;
 
 namespace SkillShare.Application.DTOs
 {
     public class UserResponseDto
     {
-        
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public UserRoles Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+        public bool IsVerified { get; set; }
+        public int ReputationScore { get; set; }
     }
 }
