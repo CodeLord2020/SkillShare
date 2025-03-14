2fa0da238cda92b622f6101f0e364cf58c2b81a0
Rasheed Babatunde <brasheed240@gmail.com>
2025-03-14 17:54:00 +0100

    UserRegistrationDto

--- Application/DTOs/UserRegistrationDto.cs
+++ Application/DTOs/UserRegistrationDto.cs
@@ -7,6 +7,9 @@ namespace SkillShare.Application.DTOs
 {
     public class UserRegistrationDto
     {
-        
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
     }
 }
